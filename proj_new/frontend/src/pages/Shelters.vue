36b827b3250e15b8ad6c7df58bcc0122653b0f13
Yee Lee <a26195828963@gmail.com>
2025-11-29 23:29:26 +0800

    1130_雙引號衝突

--- proj_new/frontend/src/pages/Shelters.vue
+++ proj_new/frontend/src/pages/Shelters.vue
@@ -77,8 +77,8 @@
                 <span 
                   :class="[
                     shelter.verified 
-                      ? "bg-green-100 text-green-800" 
-                      : "bg-gray-100 text-gray-800"
+                      ? 'bg-green-100 text-green-800' 
+                      : 'bg-gray-100 text-gray-800'
                   ]"
                   class="inline-flex items-center px-2 py-1 rounded text-xs font-medium"
                 >
